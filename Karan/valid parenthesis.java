9ce0a4ea6a12395f6d4c634f56e90f95eeb2ace8
KaranMern <122736820+KaranMern@users.noreply.github.com>
2023-07-06 23:46:14 +0930

    valid parenthesis.java

---
+++ Karan/valid parenthesis.java
@@ -0,0 +1,53 @@
+import java.util.Stack;
+class Main{ 
+    public static void main(String args[])
+    {
+        String str="{[()]}";
+        Stack<Character> st=new Stack<Character>();
+        for(int i=0;i<str.length();i++)
+        {
+            char ch=str.charAt(i);
+            if(ch=='{'||ch=='('||ch=='[')
+            {
+                st.push(ch);
+            }
+                else{
+                    if(st.isEmpty()){
+                        System.out.println("INVALID");
+                    }
+                    else if(ch=='}')
+                    {
+                        if(st.peek()=='{')
+                            st.pop();
+                        else{
+                            System.out.println("INVALID");
+                            break;
+                        }
+                    }
+                    else if(ch==']')
+                    {
+                        if(st.peek()=='[')
+                            st.pop();
+                        else{
+                            System.out.println("INVALID");
+                            break;
+                        }
+                    }
+                    else if(ch==')')
+                    {
+                        if(st.peek()=='(')
+                            st.pop();
+                        else{
+                            System.out.println("INVALID");
+                            break;
+                        }
+                    }
+                }
+            }
+        
+        if(st.size()==0)
+            System.out.println("PARENTESIS VALID");
+        else
+            System.out.println(st);
+    }
+}
